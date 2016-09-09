7fdb916fd289e1a03135c3f245966bcb2301eee8
Michael <michael.fondue@gmail.com>
2016-09-09 17:34:16 +0200

    Kamera added in Form

--- cameratest/cameratest/cameratest/BugReporting.xaml.cs
+++ cameratest/cameratest/cameratest/BugReporting.xaml.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Plugin.Media;
 using Xamarin.Forms;
 
 namespace cameratest
@@ -18,5 +18,34 @@ namespace cameratest
         //{
         //    await Navigation.PushAsync(new BugReporting());
         //}
+        async void takePhoto(object sender, EventArgs e)
+        {
+
+            if (CrossMedia.Current.IsCameraAvailable && CrossMedia.Current.IsTakePhotoSupported)
+            {
+                // Supply media options for saving our photo after it's taken.
+                var mediaOptions = new Plugin.Media.Abstractions.StoreCameraMediaOptions
+                {
+                    Directory = "cameratest",
+                    Name = $"{DateTime.UtcNow}.jpg"
+                };
+
+                // Take a photo and save into cameratest.
+                var file = await CrossMedia.Current.TakePhotoAsync(mediaOptions);
+
+                if (file == null)
+                    return;
+
+                DisplayAlert("File Location", file.Path, "OK");
+
+                image.Source = ImageSource.FromStream(() =>
+                {
+                    var stream = file.GetStream();
+                    file.Dispose();
+                    return stream;
+                });
+
+            }
+        }
     }
 }
