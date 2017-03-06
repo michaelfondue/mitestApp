ad04a0362536421cdf99fe06c4fb57d084c365df
Michael <michael.fondue@gmail.com>
2017-03-06 12:10:05 +0100

    Appname auf ServiceApp geändert

--- cameratest/cameratest/cameratest.Droid/MainActivity.cs
+++ cameratest/cameratest/cameratest.Droid/MainActivity.cs
@@ -9,7 +9,7 @@ using Android.OS;
 
 namespace cameratest.Droid
 {
-    [Activity(Label = "cameratest", Icon = "@drawable/icon", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Icon = "@drawable/icon", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsApplicationActivity
     {
         protected override void OnCreate(Bundle bundle)
